528a47adb8491826917bb136e1ea027187da9a90
Breakkk <951465997@qq.com>
2017-06-24 22:50:37 +0800

    modify the comments

--- Detect.h
+++ Detect.h
@@ -1,9 +1,9 @@
 #ifndef DETECT_H
 #define DETECT_H
 /********************************************************************
-//      这个类为一个抽象类，在以后可以增加不同的Detect来检查两个文件的相
-//      似度。
-//
+    This class is an abstract class, we can redefine the function
+in the dericed class so that we can provide different method to compute
+the checking.
 ********************************************************************/
 class Detect
 {
